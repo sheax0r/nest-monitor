b7eb5df7e3f7fae040e7982ea8fa9fa1c6648e9c
Mike Shea <michael.shea@heroku.com>
2016-02-12 00:00:38 -0500

    Deal with poorly named 'type' column

--- lib/nest-monitor/model/event.rb
+++ lib/nest-monitor/model/event.rb
@@ -1,3 +1,5 @@
 require 'active_record'
 class Event < ActiveRecord::Base
+  # Whoops, maybe I shouldn't have named that column "type" after all ...
+  self.inheritance_column = "inheritance_type"
 end
