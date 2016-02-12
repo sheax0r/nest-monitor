6bd39a4a7ff9f5186f2ee65d1f74d821c5d4c38e
Mike Shea <michael.shea@heroku.com>
2016-02-11 19:38:56 -0500

    fix typo

--- lib/nest-monitor/jobs/restart_nest.rb
+++ lib/nest-monitor/jobs/restart_nest.rb
@@ -7,7 +7,7 @@ module NestMonitor
     class RestartNest
       include SuckerPunch::Job
       def perform
-        Scrolls.log(class: self.class, at: perform) do
+        Scrolls.log(class: self.class, at: 'perform') do
           nest = NestThermostat::Nest.new(email:ENV['NEST_USER'], password: ENV['NEST_PASSWORD'])
           target = nest.temp
           nest.temp = nest.current_temp - 2
